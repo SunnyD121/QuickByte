c3223a254e91a6d9c984bfb66b2302731e51888b
swilery <swilery@gmail.com>
2018-07-28 15:08:53 -0500

    Perhaps

--- Angular/src/app/routing.ts
+++ Angular/src/app/routing.ts
@@ -4,7 +4,7 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/homepage/homepage.component';
 import { LoginPageComponent } from './components/loginpage/loginpage.component';
 import { SignupPageComponent } from './components/signuppage/signuppage.component';
-import { ErrorPage404Component } from './components/errorpage404/errorpage404.component';
+// import { ErrorPage404Component } from './components/errorpage404/errorpage404.component';
 
 
 export const approutes: Routes = [
@@ -39,6 +39,7 @@ export const approutes: Routes = [
         //This designates how a user can be taken back to this page
         //should they arrive at a 404
         path: '**',
-        component: ErrorPage404Component
+        // component: ErrorPage404Component
+        component: HomePageComponent
     }
 ]
