3ea6f5c3abe342230bd9a194067a630139dfb1aa
Chris Parsons <chris@cparsonsdesign.com>
2018-08-03 11:26:40 -0500

    more changes y'all

--- src/main/java/com/revature/overknight/servlets/CreatePostServlet.java
+++ src/main/java/com/revature/overknight/servlets/CreatePostServlet.java
@@ -1,32 +1,19 @@
 package com.revature.overknight.servlets;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.UUID;
+import java.io.PrintWriter;
+import java.io.StringReader;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
-
-
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
-import com.amazonaws.services.mediastoredata.model.Item;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.revature.overknight.utils.AWSObjectIO;
-
-import gherkin.deps.com.google.gson.Gson;
 /**
  * Servlet implementation class CreatePostServlet
  */
@@ -56,51 +43,28 @@ public class CreatePostServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        AWSObjectIO ai = new AWSObjectIO();
+    	 //SETUP
+		String json = request.getReader().readLine();
+        JsonReader reader = Json.createReader(new StringReader(json));
+
+        // READ INCOMING JSON OBJECT
+        JsonObject personObject = reader.readObject();
+        reader.close();
+
+		//GET INFO
+        String username = personObject.getString("username");
+		String text = personObject.getString("text");
+		
+		//SETUP REPLY
+		response.setContentType("text/json");
+		PrintWriter out = response.getWriter();
+    	
+    	// UPLOAD THE FILE TO S3
+    	AWSObjectIO ai = new AWSObjectIO();
         AWSObjectIO.uploadFile(request, response);
+        
+        // ADD POST TO THE DATABASE
 
-    	/*ServletFileUpload sf = new ServletFileUpload(new DiskFileItemFactory());
-        AmazonS3 s3client = AmazonS3ClientBuilder.standard().withRegion("us-east-1")
-                .withCredentials(new EnvironmentVariableCredentialsProvider())
-                .build();
-        String bucketname = "quickbytes3";
-//        s3client.createBucket(bucketname);
-        response.setContentType("text");
-        String fileKey = UUID.randomUUID().toString();
-        Item jsonItem = null;
-        try
-        {
-            List<FileItem> files = sf.parseRequest(request);
-            for(FileItem item: files)
-            {
-                if (item.isFormField())
-                {
-                    String fieldname = item.getFieldName();
-                    String fieldvalue = item.getString();
-                    if(fieldname.equals("subItem"))
-                    {
-                        Gson gson = new Gson();
-                        jsonItem = gson.fromJson(fieldvalue,Item.class);
-                    }
-                }
-                else if(!item.getName().equals("null") && item.getName() != null)
-                {
-                    fileKey = fileKey+item.getName();
-                    System.out.println(fileKey);
-                    InputStream is = item.getInputStream();
-                    s3client.putObject(new PutObjectRequest(bucketname, fileKey,is,new ObjectMetadata())
-                            .withCannedAcl(CannedAccessControlList.PublicRead));
-                    is.close();
-                }
-
-            }
-        }
-        catch (Exception e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-
-        }*/
 
     }
 
