8604924c2b9bdc08f44852ea9c399f2600152683
Eric <opaqueradiance@gmail.com>
2018-08-05 21:04:19 -0500

    selenium commented for deployment

--- src/test/java/com/revature/overknight/frontend/Tests/QuickByteDriverTest.java
+++ src/test/java/com/revature/overknight/frontend/Tests/QuickByteDriverTest.java
@@ -20,163 +20,163 @@ import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.*;
 
 public class QuickByteDriverTest {
-
-    public static WebDriver driver;
-    public final String url = "http://localhost:4200";
-    WebDriverWait wait;
-
-    @BeforeClass
-    public void beforeClass(){
-        wait = new WebDriverWait(driver, 7);        //explicit (configuration)
-    }
-
-    @BeforeTest
-    public void setup(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-
-        //Wait 3 seconds for an element to load before auto-failing
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);     //implicit
-        driver.get(url);
-    }
-
-    @AfterTest
-    public void tearDown() throws InterruptedException{
-//        Thread.sleep(500);
-        driver.quit();
-    }
-
-    @Test(priority = 1, groups = "smoke")
-    public void smokeTest(){
-        assertEquals(driver.getTitle(), "QuickByte");
-    }
-
-    @Test(priority = 3, dependsOnGroups = "smoke")
-    public void testLoginPage() throws InterruptedException{
-        LoginPagePom loginPom = new LoginPagePom(driver);
-        HomePagePom homePom = new HomePagePom(driver);
-        assertEquals(homePom.getLogInOutButtonText(), "Log In");    //smoke
-        homePom.clickLogInOutButton();
-        String currentUrl = driver.getCurrentUrl();
-        assertEquals(currentUrl, url + "/#/loginpage"); //smoke
-
-        //Negative Test
-        loginPom.inputUsername("Apples");
-        loginPom.inputPassword("Bananas");
-        loginPom.clickLoginButton();
-        String getResponse = loginPom.getWaitMessage();
-        int i = 0;
-        while(!getResponse.equals("") && i < 4){    //allows 4 * 500ms (2s) for the login to get a response
-            getResponse = loginPom.getWaitMessage();
-            i++;
-            Thread.sleep(500);
-
-        }
-        if (i == 4) fail("Login took too long to get a response.");
-        assertEquals(loginPom.getErrorMessage(), "Username or Password is incorrect");
-
-        //Positive Test
-        loginPom.clearInput();
-
-        loginPom.inputUsername("erics");
-        loginPom.inputPassword("erics");
-        loginPom.clickLoginButton();
-        getResponse = loginPom.getWaitMessage();
-        i = 0;
-        while(!getResponse.equals("") && i < 4 && driver.getCurrentUrl().equals(currentUrl)){    //allows 4 * 500ms (2s) for the login to get a response
-            try {getResponse = loginPom.getWaitMessage();}
-            catch(NoSuchElementException e){getResponse = ""; Logger.log(this.getClass(), e);}
-            i++;
-            Thread.sleep(500);
-        }
-        if (i == 4) fail("Login took too long to get a response.");
-        assertEquals(driver.getCurrentUrl(), url + "/#/homepage");
-    }
-
-    @Test(priority = 10, dependsOnGroups = "smoke")
-    public void logOutTest(){
-        //driver.findElement(By.xpath("/html/body/app-root/html/body/app-homepage/app-nav-bar/nav/div/ul[2]/li[2]/a")).click();
-        String currentUrl = driver.getCurrentUrl();
-        assertEquals(currentUrl, url + "/#/homepage"); //smoke
-
-        HomePagePom homePom = new HomePagePom(driver);
-        assertEquals(homePom.getLogInOutButtonText(), "Log Out");
-        homePom.clickLogInOutButton();
-        if(ExpectedConditions.alertIsPresent().apply(driver)!=null){
-            driver.switchTo().alert().accept();
-        }
-        driver.navigate().refresh();    //TODO: This is a workaround to pass the test. Remove it.
-        Logger.log(this.getClass(), "Used a workaround to pass a Test.", Logger.Severity.WARN);
-        assertEquals(homePom.getLogInOutButtonText(), "Log In");
-    }
-
-    @Test(priority = 2, dependsOnGroups = "smoke")
-    public void signUpTest(){
-        HomePagePom homePom = new HomePagePom(driver);
-        String currentUrl = driver.getCurrentUrl();
-        assertEquals(currentUrl, url + "/#/homepage"); //smoke
-        assertEquals(homePom.getSignUpOrPostButtonText(), "Sign Up");
-        homePom.clickSignUpOrPostButton();
-        currentUrl = driver.getCurrentUrl();
-        assertEquals(currentUrl, url + "/#/signuppage");
-        SignUpPagePom signPom = new SignUpPagePom(driver);
-
-        //Negative Tests
-        assertFalse(signPom.getErrorMessageUsernameIsDisplayed());
-        assertFalse(signPom.getErrorMessagePasswordIsDisplayed());
-        assertFalse(signPom.getErrorMessageCreditCardIsDisplayed());
-        signPom.inputUsername("erics");
-        signPom.inputPassword1("password 1");
-        signPom.inputPassword2("password 2");
-        signPom.inputCreditCard("1111111111111111");
-        signPom.loseFocus();
-        assertTrue(signPom.getErrorMessageUsernameIsDisplayed());
-        assertTrue(signPom.getErrorMessagePasswordIsDisplayed());
-        assertTrue(signPom.getErrorMessageCreditCardIsDisplayed());
-
-        signPom.clearInput();
-        //Positive Test
-        final String user = "TestUser";
-        signPom.inputUsername(user);
-        signPom.inputPassword1(user);
-        signPom.inputPassword2(user);
-        signPom.inputCreditCard("4388576018410707");
-        signPom.loseFocus();
-        assertFalse(signPom.getErrorMessageUsernameIsDisplayed());
-        assertFalse(signPom.getErrorMessagePasswordIsDisplayed());
-        assertFalse(signPom.getErrorMessageCreditCardIsDisplayed());
-
-        //TODO: I would like to click submit, but I can't delete my testuser, which would break UNIQUE constraint upon re-test
-//        signPom.clickSubmitButton();
-        driver.navigate().back();
-        assertEquals(driver.getCurrentUrl(), url + "/#/homepage");
-    }
-
-    @Test(priority = 4, dependsOnGroups = "smoke")
-    public void createPostTest(){
-        HomePagePom homePom = new HomePagePom(driver);
-        String currentUrl = driver.getCurrentUrl();
-        assertEquals(currentUrl, url + "/#/homepage"); //smoke
-        assertEquals(homePom.getSignUpOrPostButtonText(), "New Post");
-        homePom.clickSignUpOrPostButton();
-        currentUrl = driver.getCurrentUrl();
-        assertEquals(currentUrl, url + "/#/create-post");
-
-        CreatePostPagePom postPom = new CreatePostPagePom(driver);
-        postPom.inputTitle("TestTitle");
-        postPom.inputRecipe("TestRecipe");
-        postPom.inputComment("TestComment");
-//        postPom.clickPictureButton();
-//        postPom.clickPreviewButton();
-//        assertEquals("Actual", "Expected");
-
-        driver.navigate().back();
-        assertEquals(driver.getCurrentUrl(), url + "/#/homepage");
-
-    }
-
-
-
-
+//
+//    public static WebDriver driver;
+//    public final String url = "http://localhost:4200";
+//    WebDriverWait wait;
+//
+//    @BeforeClass
+//    public void beforeClass(){
+//        wait = new WebDriverWait(driver, 7);        //explicit (configuration)
+//    }
+//
+//    @BeforeTest
+//    public void setup(){
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+//        driver = new ChromeDriver();
+//
+//        //Wait 3 seconds for an element to load before auto-failing
+//        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);     //implicit
+//        driver.get(url);
+//    }
+//
+//    @AfterTest
+//    public void tearDown() throws InterruptedException{
+////        Thread.sleep(500);
+//        driver.quit();
+//    }
+//
+//    @Test(priority = 1, groups = "smoke")
+//    public void smokeTest(){
+//        assertEquals(driver.getTitle(), "QuickByte");
+//    }
+//
+//    @Test(priority = 3, dependsOnGroups = "smoke")
+//    public void testLoginPage() throws InterruptedException{
+//        LoginPagePom loginPom = new LoginPagePom(driver);
+//        HomePagePom homePom = new HomePagePom(driver);
+//        assertEquals(homePom.getLogInOutButtonText(), "Log In");    //smoke
+//        homePom.clickLogInOutButton();
+//        String currentUrl = driver.getCurrentUrl();
+//        assertEquals(currentUrl, url + "/#/loginpage"); //smoke
+//
+//        //Negative Test
+//        loginPom.inputUsername("Apples");
+//        loginPom.inputPassword("Bananas");
+//        loginPom.clickLoginButton();
+//        String getResponse = loginPom.getWaitMessage();
+//        int i = 0;
+//        while(!getResponse.equals("") && i < 4){    //allows 4 * 500ms (2s) for the login to get a response
+//            getResponse = loginPom.getWaitMessage();
+//            i++;
+//            Thread.sleep(500);
+//
+//        }
+//        if (i == 4) fail("Login took too long to get a response.");
+//        assertEquals(loginPom.getErrorMessage(), "Username or Password is incorrect");
+//
+//        //Positive Test
+//        loginPom.clearInput();
+//
+//        loginPom.inputUsername("erics");
+//        loginPom.inputPassword("erics");
+//        loginPom.clickLoginButton();
+//        getResponse = loginPom.getWaitMessage();
+//        i = 0;
+//        while(!getResponse.equals("") && i < 4 && driver.getCurrentUrl().equals(currentUrl)){    //allows 4 * 500ms (2s) for the login to get a response
+//            try {getResponse = loginPom.getWaitMessage();}
+//            catch(NoSuchElementException e){getResponse = ""; Logger.log(this.getClass(), e);}
+//            i++;
+//            Thread.sleep(500);
+//        }
+//        if (i == 4) fail("Login took too long to get a response.");
+//        assertEquals(driver.getCurrentUrl(), url + "/#/homepage");
+//    }
+//
+//    @Test(priority = 10, dependsOnGroups = "smoke")
+//    public void logOutTest(){
+//        //driver.findElement(By.xpath("/html/body/app-root/html/body/app-homepage/app-nav-bar/nav/div/ul[2]/li[2]/a")).click();
+//        String currentUrl = driver.getCurrentUrl();
+//        assertEquals(currentUrl, url + "/#/homepage"); //smoke
+//
+//        HomePagePom homePom = new HomePagePom(driver);
+//        assertEquals(homePom.getLogInOutButtonText(), "Log Out");
+//        homePom.clickLogInOutButton();
+//        if(ExpectedConditions.alertIsPresent().apply(driver)!=null){
+//            driver.switchTo().alert().accept();
+//        }
+//        driver.navigate().refresh();    //TODO: This is a workaround to pass the test. Remove it.
+//        Logger.log(this.getClass(), "Used a workaround to pass a Test.", Logger.Severity.WARN);
+//        assertEquals(homePom.getLogInOutButtonText(), "Log In");
+//    }
+//
+//    @Test(priority = 2, dependsOnGroups = "smoke")
+//    public void signUpTest(){
+//        HomePagePom homePom = new HomePagePom(driver);
+//        String currentUrl = driver.getCurrentUrl();
+//        assertEquals(currentUrl, url + "/#/homepage"); //smoke
+//        assertEquals(homePom.getSignUpOrPostButtonText(), "Sign Up");
+//        homePom.clickSignUpOrPostButton();
+//        currentUrl = driver.getCurrentUrl();
+//        assertEquals(currentUrl, url + "/#/signuppage");
+//        SignUpPagePom signPom = new SignUpPagePom(driver);
+//
+//        //Negative Tests
+//        assertFalse(signPom.getErrorMessageUsernameIsDisplayed());
+//        assertFalse(signPom.getErrorMessagePasswordIsDisplayed());
+//        assertFalse(signPom.getErrorMessageCreditCardIsDisplayed());
+//        signPom.inputUsername("erics");
+//        signPom.inputPassword1("password 1");
+//        signPom.inputPassword2("password 2");
+//        signPom.inputCreditCard("1111111111111111");
+//        signPom.loseFocus();
+//        assertTrue(signPom.getErrorMessageUsernameIsDisplayed());
+//        assertTrue(signPom.getErrorMessagePasswordIsDisplayed());
+//        assertTrue(signPom.getErrorMessageCreditCardIsDisplayed());
+//
+//        signPom.clearInput();
+//        //Positive Test
+//        final String user = "TestUser";
+//        signPom.inputUsername(user);
+//        signPom.inputPassword1(user);
+//        signPom.inputPassword2(user);
+//        signPom.inputCreditCard("4388576018410707");
+//        signPom.loseFocus();
+//        assertFalse(signPom.getErrorMessageUsernameIsDisplayed());
+//        assertFalse(signPom.getErrorMessagePasswordIsDisplayed());
+//        assertFalse(signPom.getErrorMessageCreditCardIsDisplayed());
+//
+//        //TODO: I would like to click submit, but I can't delete my testuser, which would break UNIQUE constraint upon re-test
+////        signPom.clickSubmitButton();
+//        driver.navigate().back();
+//        assertEquals(driver.getCurrentUrl(), url + "/#/homepage");
+//    }
+//
+//    @Test(priority = 4, dependsOnGroups = "smoke")
+//    public void createPostTest(){
+//        HomePagePom homePom = new HomePagePom(driver);
+//        String currentUrl = driver.getCurrentUrl();
+//        assertEquals(currentUrl, url + "/#/homepage"); //smoke
+//        assertEquals(homePom.getSignUpOrPostButtonText(), "New Post");
+//        homePom.clickSignUpOrPostButton();
+//        currentUrl = driver.getCurrentUrl();
+//        assertEquals(currentUrl, url + "/#/create-post");
+//
+//        CreatePostPagePom postPom = new CreatePostPagePom(driver);
+//        postPom.inputTitle("TestTitle");
+//        postPom.inputRecipe("TestRecipe");
+//        postPom.inputComment("TestComment");
+////        postPom.clickPictureButton();
+////        postPom.clickPreviewButton();
+////        assertEquals("Actual", "Expected");
+//
+//        driver.navigate().back();
+//        assertEquals(driver.getCurrentUrl(), url + "/#/homepage");
+//
+//    }
+//
+//
+//
+//
 
 }
